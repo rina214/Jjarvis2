35eb0a052099506b18726136baf7d0c71def2b68
jiyoonlee <sundaeluv98@gmail.com>
2020-07-08 12:48:55 +0900

    2020-07-08 (프래그먼트 전환), (팝업)_DB 마무리 필요.(막)

---
+++ app/src/main/java/com/example/jjarvis2/fixinfo.java
@@ -0,0 +1,48 @@
+package com.example.jjarvis2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+
+public class fixinfo extends Activity {
+    DatabaseReference db;
+    String userUid;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    EditText wannabe;
+    Button btn_ok;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        db = FirebaseDatabase.getInstance().getReference();
+        userUid = user.getUid();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.fixinfo_activity);
+
+        btn_ok = (Button) findViewById(R.id.btn_ok);
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = wannabe.getText().toString();
+                Intent intent = new Intent();
+                intent.putExtra("DATA", data);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+        wannabe= (EditText)findViewById(R.id.wannabe);
+    }
+}
